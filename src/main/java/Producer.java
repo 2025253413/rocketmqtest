9d6e579de63127f1af90c4825ce872b92aa56efd
liuzhifeng-001 <2736487308@qq.com>
2023-06-17 09:02:10 +0800

    Initial commit

---
+++ src/main/java/Producer.java
@@ -0,0 +1,10 @@
+public class Producer {
+    public static void main(String[] args) throws Exception {
+        Producer producer=new Producer();
+//        producer.setNameSreAddr("192.168.200.143");
+//        producer.
+
+
+    }
+
+}
